ba1e17e600f5ec89acefce801c8d5c149d897937
phunkyjon.eth <83514596+StarKeyJON@users.noreply.github.com>
2021-12-20 14:09:55 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ function Footer() {
         <div>
             (©2021 PhunkFinder - Beta V-0.2.0; All Rights Not Reserved; CopyLeft Permissible - Freedom 3)<img className='footer_img' src='./phunk9249.png'alt='PhunkyJON.eth' width="40" height="40"/>
         </div>
+        <div>The Phelda game is an experimental hack by me as I am beginning to learn game development, with credits attributed to the creator of the game during gameplay! I hope you enjoy! More updates to come along with the ability to select your phunk and customized characters.</div>
     )
 }
 
